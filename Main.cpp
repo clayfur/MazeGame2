5f77a0fe60b38565f1cace1e02588620b9b26d1f
clayfur <48552072+clayfur@users.noreply.github.com>
2024-09-01 21:47:54 +0530

    Updated code

--- Main.cpp
+++ Main.cpp
@@ -4,30 +4,30 @@
 #include "Code.h"
 using namespace std;
 int main() {
-  int m = 0,n = 0;
+  int m = 0, n = 0;
   bool eye;
   mojave moveto;
   std::cerr << "To enable eye comfort mode press 1(system memory consumption increased)" << '\n';
   cin >> eye;
-  for(int i = 0;i<30;i++){
-    for(int j = 0;j<120;j++){
-        cout<<(char)marp[0][i][j];
-      }
-      cout<<endl;
+  for (int i = 0;i < 30;i++) {
+    for (int j = 0;j < 120;j++) {
+      cout << (char)marp[0][i][j];
     }
-    cout<<'\a';
-    system("pause");
-    system("color e3");
-      system("cls");
-        for(int i = 0 ; i < 30 ; i++){
-            for(int j = 0 ;j < 120 ; j++){
-              std::cout << (char)marp[0][i][j];
-        }
-    cout <<endl;
+    cout << endl;
+  }
+  cout << '\a';
+  system("pause");
+  system("color e3");
+  system("cls");
+  for (int i = 0; i < 30; i++) {
+    for (int j = 0;j < 120; j++) {
+      std::cout << (char)marp[0][i][j];
+    }
+    cout << endl;
   }
   cout << "\a";
   int mover;
-  bool paste,end,ended=0;
+  bool paste, end, ended = 0;
   do {
     paste = 0;
     mover = getch();
@@ -36,49 +36,50 @@ int main() {
       moveto.keys();
     }
     kpress++;
-    switch(mover){
-      case ' ':
-        cout<<"space pressed";
-        moveto.cannon();
-        moveto.lock();
-        break;
-      case '/':
-        moveto.code();
-        break;
-      case 'f':
-      case 'F':
-        moveto.fix();
-        break;
-      case 'a':
-      case 'A':
-      case 75:
-        paste = moveto.left();
-        break;
-      case 'd':
-      case 'D':
-      case 77:
-        paste = moveto.right();
-        break;
-      case 'w':
-      case 'W':
-      case 72:
-        paste = moveto.up();
-        break;
-      case 's':
-      case 'S':
-      case 80:
-        paste = moveto.down();
-        break;
+    switch (mover) {
+    case ' ':
+      cout << "space pressed";
+      moveto.cannon();
+      moveto.lock();
+      break;
+    case '/':
+      moveto.code();
+      break;
+    case 'f':
+    case 'F':
+      moveto.fix();
+      break;
+    case 'a':
+    case 'A':
+    case 75:
+      paste = moveto.left();
+      break;
+    case 'd':
+    case 'D':
+    case 77:
+      paste = moveto.right();
+      break;
+    case 'w':
+    case 'W':
+    case 72:
+      paste = moveto.up();
+      break;
+    case 's':
+    case 'S':
+    case 80:
+      paste = moveto.down();
+      break;
     }
-    if (paste==1) {
-      if (eye!=1){
-      system("cls");
-      cout<<k[0][0];
+    if (paste == 1) {
+      if (eye != 1) {
+        //system("cls");
+        cout << "\x1b[H";
+        cout << k[0][0];
+      }
+      else { cout << '\n'; }
+      // cout<<'\r'<<endl<<k[0][0]<<'\r';
     }
-      else{ cout << '\n';}
-     // cout<<'\r'<<endl<<k[0][0]<<'\r';
-}
-}while(moveto.door()!=1);
-std::cout << "Now press any key to exit" << '\n';
-getch();
+  } while (moveto.door() != 1);
+  std::cout << "Now press any key to exit" << '\n';
+  getch();
 }
